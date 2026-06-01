4b719cf38bcaca2903b667710f3d27e22c77f576
soklong <soklongyoung@gmail.com>
2026-06-01 18:32:05 +0700

    Use file cache, remove cache:clear from boot

--- docker/start.sh
+++ docker/start.sh
@@ -1,17 +1,12 @@
 #!/usr/bin/env bash
 set -o errexit
 
-# Clear any stale cache first
-php artisan config:clear
-php artisan cache:clear
+php artisan config:clear || true
 
-# Run migrations
 php artisan migrate --force
 
-# Now cache with real env vars present
 php artisan config:cache
 php artisan route:cache
 php artisan view:cache
 
-# Start services
 /usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf
